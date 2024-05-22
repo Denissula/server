e4d22bc060ca2329ab573d11b8ffd11f76b73002
Denis Sula <denissula16@gmail.com>
2024-05-22 14:55:59 +0200

    E-Commerce Backend

---
+++ Dtos/RefreshTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Dtos;
+
+public class RefreshTokenDto
+{
+    public string RefreshToken { get; set; } = null!;
+    public string Token { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
